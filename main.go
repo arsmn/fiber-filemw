dc989204282324acb142edbd95b0531cc3072029
arsmn <arsmn1996@gmail.com>
2020-04-20 23:28:51 +0430

    set Content-Type header

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package filemw
 
 import (
+	"io"
+	"io/ioutil"
 	"log"
+	"mime"
 	"net/http"
+	"os"
 	"path/filepath"
 	"strings"
 
@@ -63,9 +67,53 @@ func New(config ...Config) func(*fiber.Ctx) {
 				return
 			}
 
+			contentType, err := detectContentType(file, stat)
+			if err != nil {
+				cfg.ErrorHandler(c, err)
+				return
+			}
+
+			c.Fasthttp.SetContentType(contentType)
 			c.Fasthttp.SetBodyStream(file, int(stat.Size()))
 			return
 		}
 		c.Next()
 	}
+}
+
+func detectContentType(f http.File, fileInfo os.FileInfo) (string, error) {
+	ext := getFileExtension(fileInfo.Name())
+	contentType := mime.TypeByExtension(ext)
+	if len(contentType) == 0 {
+		data, err := readFileHeader(f)
+		if err != nil {
+			return "", err
+		}
+		contentType = http.DetectContentType(data)
+	}
+
+	return contentType, nil
+}
+
+func getFileExtension(path string) string {
+	n := strings.LastIndexByte(path, '.')
+	if n < 0 {
+		return ""
+	}
+
+	return path[n:]
+}
+
+func readFileHeader(f http.File) ([]byte, error) {
+	r := io.Reader(f)
+	lr := &io.LimitedReader{
+		R: r,
+		N: 512,
+	}
+	data, err := ioutil.ReadAll(lr)
+	if _, err := f.Seek(0, 0); err != nil {
+		return nil, err
+	}
+
+	return data, err
 }
